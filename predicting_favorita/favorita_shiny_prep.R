f76ecd5b1554b38c8af7fb234a99606c3303e82f
DH <compassioncrumb@gmail.com>
2021-12-10 21:37:39 -0700

    Add app_navbar; Update favorita_shiny_prep
    Created Shiny App with navbar Interface. Added Functionality to favorita_shiny_prep

--- predicting_favorita/favorita_shiny_prep.R
+++ predicting_favorita/favorita_shiny_prep.R
@@ -3,6 +3,11 @@ library(tidyverse)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(stringr)
+library(corrgram)
+library(Hmisc)
+library(performanceEstimation)
+library(zoo)
 
 # Shiny Project Ideas --------------------------------
 
@@ -24,14 +29,58 @@ library(ggplot2)
 # - Where is Ecuador ?
 # - Details on the Earthquake
 
+# Earthquake ------------------------------------------
+
+# magnitude: 7.8
+# date: 2016-04-16
 
  
 # Read in Data ----------------------------------------
 base_path = "~/DSA/R_project/Favorita_Shiny_R/predicting_favorita/"
-data_transactions = read.csv(paste(base_path,"store-sales-time-series-forecasting/transactions.csv", sep =""))
-data_oil = read.csv(paste(base_path,"store-sales-time-series-forecasting/oil.csv", sep =""))
-data_holiday_events = read.csv(paste(base_path,"store-sales-time-series-forecasting/holidays_events.csv", sep =""))
-data_stores = read.csv(paste(base_path,"store-sales-time-series-forecasting/stores.csv", sep =""))
+data_transactions = read_csv(paste(base_path,"store-sales-time-series-forecasting/transactions.csv", sep =""))
+data_oil = read_csv(paste(base_path,"store-sales-time-series-forecasting/oil.csv", sep =""))
+data_holiday_events = read_csv(paste(base_path,"store-sales-time-series-forecasting/holidays_events.csv", sep =""))
+data_stores = read_csv(paste(base_path,"store-sales-time-series-forecasting/stores.csv", sep =""))
+data_items = read_csv(paste(base_path, "store-sales-time-series-forecasting/items.csv", sep =""))
+data_train = read_csv(paste(base_path, "store-sales-time-series-forecasting/train.csv", sep =""))
+
+#----------------------------------------------------------------------------
+data_store_plus = merge(x=data_transactions, y=data_stores, 
+                        by.x = "store_nbr", by.y = "store_nbr", x.all=TRUE)
+
+# Chunking Training Data by Year --------------------------------------------
+# 
+# data_train <-  data_train %>%
+#   mutate(date=as.Date(date, format="%Y-%m-%d"))
+train_years <- unique(format(data_train$date,"%Y"))
+
+for (y in train_years) {
+  # data_train_yr[y] <-  y
+  temp_dat <- data_train %>%
+    filter(date >= as.POSIXct(str_glue("{y}-01-01"))) %>%
+    filter(date <= as.POSIXct(str_glue("{y}-12-31")))
+
+    assign(str_glue("data_train_{y}"), temp_dat)
+    data_train_yr$yr <- temp_dat
+}
+
+data_train_yr <- list()
+for (y in train_years) {
+  data_train_yr[[y]] <- str_glue("data_train_{y}")
+  
+}
+
+# Separate Training into pre Earthquake and post quake ------------------------
+quake_date = "2016-04-16"
+pre_quake_train <- data_train %>%
+  filter(date < as.POSIXct(quake_date))
+
+
+post_quake_train <- data_train %>%
+  filter(date > as.POSIXct(quake_date))
+
+quake_day_train <- data_train %>%
+  filter(date > as.POSIXct(quake_date))
 
 
 # Create Primary DataFrame ----------------------------------------------------
@@ -41,8 +90,15 @@ fav_df <- merge(x=data_transactions, y=data_stores,
           mutate(holiday_applies = if_else((locale == "National"), TRUE,
                           if_else((locale == "Regional") & (locale_name == state), TRUE,
                           if_else((locale == "Local") & (locale_name == city), TRUE, false=FALSE)))) %>% 
-          merge(data_oil, by = "date", all.x=TRUE, incomparables=NA)
+          merge(data_oil, by = "date", all.x=TRUE, incomparables=NA) %>%
+          mutate(date=as.Date(date, format="%Y-%m-%d")) %>%
+          mutate(day_of_week = weekdays(date)) %>%
+          rename(store_type = type.x) %>%
+          rename(holiday_type = type.y)
 
+
+
+write.csv(fav_df, paste(base_path, "store-sales-time-series-forecasting/fav_main_df.csv", sep=""))
 
           #- todo: holidays events mutate applicable holiday events
   
@@ -50,6 +106,70 @@ fav_df <- merge(x=data_transactions, y=data_stores,
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
+
+data_main <-  read_csv(paste(base_path, "store-sales-time-series-forecasting/fav_main_df.csv", sep=""))
+
+#data_product <- merge(data_items, data_main, by=)
+
+for (y_dat in data_train_yr) {
+  data_product <- data_store_plus %>%
+    # incorporate product data - 
+    right_join(y_dat, by = c("store_nbr" = "store_nbr")) %>% 
+    
+    # incorporate holiday data -
+    merge(data_holiday_events, by = "date", all.x=TRUE, incomparables=NA) %>%
+    mutate(holiday_applies = if_else((locale == "National"), TRUE,
+                                     if_else((locale == "Regional") & (locale_name == state), TRUE,
+                                             if_else((locale == "Local") & (locale_name == city), TRUE, false=FALSE)))) %>% 
+    # incorporate Oil Data - 
+    merge(data_oil, by = "date", all.x=TRUE, incomparables=NA) %>%
+    # Add day of week -
+    mutate(date=as.Date(date, format="%Y-%m-%d")) %>%
+    mutate(day_of_week = weekdays(date)) %>%
+    # clean up names -
+    rename(store_type = type.x) %>%
+    rename(holiday_type = type.y)
+}
+
+# Analyze Products ------------------------------------------------------------
+
+# --- beta analysis ----
+
+pre_qk2 <- pre_quake_train %>% 
+  group_by(family) %>%
+  group_by(date, .add=TRUE) %>%
+  summarise(total_sales = sum(sales)) %>%
+  spread(family, total_sales) %>%
+  merge(data_oil, by = "date", all.x=TRUE, incomparables=NA) %>%
+  mutate(date=as.Date(date, format="%Y-%m-%d")) %>%
+  mutate(day_of_week = weekdays(date)) %>%
+  mutate(oil_price=na.approx(dcoilwtico, na.rm=FALSE)) %>%
+  fill(oil_price, .direction="up") %>%
+  select(-dcoilwtico)
+
+post_qk2 <-  post_quake_train %>% 
+  group_by(family) %>%
+  group_by(date, .add=TRUE) %>%
+  summarise(total_sales = sum(sales)) %>%
+  spread(family, total_sales) %>%
+  merge(data_oil, by = "date", all.x=TRUE, incomparables=NA) %>%
+  mutate(date=as.Date(date, format="%Y-%m-%d")) %>%
+  mutate(day_of_week = weekdays(date)) %>%
+  mutate(oil_price=na.approx(dcoilwtico, na.rm=FALSE)) %>%
+  fill(oil_price, .direction="up") %>%
+  select(-dcoilwtico)
+
+# -- analyse a single product family as time series
+
+family_use <-  "FROZEN FOODS"
+# function to get the start vector for ts 
+
+pre_quake_ts <- ts(select(pre_qk2, family_use), frequency=365, start = c(2013,1))
+# as.POSIXct("2016-04-17")-as.POSIXct("2016-01-01")+1
+post_quake_ts <- ts(select(post_qk2, family_use), frequency=365, start = c(2016,108))
+plot(post_quake_ts)
+
+
 
 # Plot Multiple Stores Over specific window frame -----------------------------
 
@@ -111,9 +231,9 @@ ggplot(select_clusters,
 
 
 
-
 #- Analyse By Day of the Week 
 #- Analyse By Weekly totals
+#-Analyse By on Promotion or not
 
 #- Oil Price Exploratory Plot
 data_oil <- data_oil %>% 
@@ -141,11 +261,55 @@ ggplot(data_long,
 # todo : combined plot of the data
 
 
-# Basic Correlation plots of average transactions over all stores with oil
+# Basic Correlation of item data with oil
+
+data_item_oil <- data_train %>%
+  # all stores
+  group_by(family) %>%
+  group_by(date, .add=TRUE) %>%
+  summarise(total_sales = sum(sales)) %>%
+  spread(family, total_sales) %>%
+  merge(data_oil, by = "date", all.x=TRUE, incomparables=NA) %>%
+  mutate(date=as.Date(date, format="%Y-%m-%d")) %>%
+  mutate(day_of_week = weekdays(date)) %>%
+  mutate(oil_price=na.approx(dcoilwtico, na.rm=FALSE)) %>%
+  fill(oil_price, .direction="up") %>%
+  select(-dcoilwtico)
+
+# data_item_oil2 <- fill(data_item_oil$dcoilwtico, .direction ="downup")
+# data_item_oil3 <- na.approx(data_item_oil$dcoilwtico)
+
+rcorr(as.matrix(select(data_item_oil,-date,-day_of_week)), type="pearson")
+
+item_oil_cor <- cor(select(data_item_oil, -date,-day_of_week))
+drinkin_and <- as.matrix(item_oil_cor[,'LIQUOR,WINE,BEER'])
+
+ggplot(data_item_oil, 
+       aes(x=date, y=total_sales, group=family, color=family)
+) + 
+  geom_line()
 
 
-# -- Shiny Feature ----
-# Basic Correlation plot of each store with oil
+corrgram(data_item_oil, order=TRUE, lower.panel=panel.shade,
+         upper.panel=panel.pie, text.panel=panel.txt,
+         main="Item Family and Oil Correlogram") 
+# Shiny Helpers ----------------------------------------
+prod_ids_opts <-  unique(data_train$id) %>% sort()
+prod_family_opts <- unique(data_train$family) %>% sort()
+max_date <- max(as.POSIXct(data_train$date)) %>% sort()
+min_date <- min(as.POSIXct(data_train$date)) %>% sort()
+
+cities_opts <- unique(fav_df$city) %>% sort()
+states_opts <- unique(fav_df$state) %>% sort()
+store_nbr_opts <- unique(fav_df$store_nbr) %>% sort()
+store_type_opts <- unique(fav_df$type.x) %>% sort()
+cluster_opts <- unique(fav_df$cluster) %>% sort()
+
+
+# -- Shiny Feature -------------------------------------
+# Basic Correlation of each store with oil
+
+
 
 # Averages for day of the week (excluding holidays)
 
@@ -154,3 +318,13 @@ ggplot(data_long,
 # calculate average store transactions correlation with 
 
 
+# Utility Functions ---------------------------------------
+
+# create vector of all dates over existing date range
+dateRangeVec <- range(as.Date(as.character(dt$p_date), format = "%Y%m%d")) 
+allDatesVec <- format(seq(from = dateRangeVec[1], 
+                          to = dateRangeVec[2], 'days'), "%Y%m%d")
+
+# filter out dates that aren't included
+outDt <- data.table(p_date = setdiff(allDatesVec, dt$p_date)) 
+#----------------------------------------------------------
---
+++ predicting_favorita/la_Futura_de_Favorita/app_navbar.R
@@ -0,0 +1,196 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+# Libraryies ---------------------------------------------------------------
+library(shiny)
+library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+# Read in Data -------------------------------------------------------------
+base_path = "~/DSA/R_project/Favorita_Shiny_R/predicting_favorita/"
+data_transactions = read.csv(paste(base_path,"store-sales-time-series-forecasting/transactions.csv", sep =""))
+data_oil = read.csv(paste(base_path,"store-sales-time-series-forecasting/oil.csv", sep =""))
+data_holiday_events = read.csv(paste(base_path,"store-sales-time-series-forecasting/holidays_events.csv", sep =""))
+data_stores = read.csv(paste(base_path,"store-sales-time-series-forecasting/stores.csv", sep =""))
+data_train = read.csv(paste(base_path, "store-sales-time-series-forecasting/train.csv", sep =""))
+
+data_main = read.csv(paste(base_path, "store-sales-time-series-forecasting/fav_main_df.csv", sep=""))
+
+# Define UI for application -------------------------------------------------
+ui <- navbarPage("Favorita Dashboard",
+               tabPanel("Product",
+                        sidebarLayout(
+                            sidebarPanel(
+                                radioButtons("plotType", "Plot type",
+                                             c("Scatter"="p", "Line"="l")
+                                )
+                            ),
+                            mainPanel(
+                                plotOutput("plot")
+                            )
+                        )
+               ),
+               tabPanel("Summary",
+                        verbatimTextOutput("summary")
+               ),
+               
+               # --------------------------------
+               tabPanel("Store",
+
+            # Sidebar with a slider input for number of bins 
+            sidebarLayout(
+                sidebarPanel(
+                    sliderInput("bins",
+                                "Number of bins:",
+                                min = 1,
+                                max = 50,
+                                value = 50), 
+                    # Input Stupbs for Favorita
+                    checkboxGroupInput("checkCity", label = h3("Cities"), 
+                                       # unique(fav_df$store_nbr) %>% sort()
+                                       choices = list("1" = 1, "2" = 2, "3" = 3, "4" = 4, "5" = 5,
+                                                      "6" = 6, "7" = 7, "53" = 53, "54" = 54),
+                                       selected = 3),
+                    
+                    checkboxGroupInput("checkStates", label = h3("States"), 
+                                       # states_vec <- unique(fav_df$state) %>% sort()
+                                       choices = list("Azuay" = "Azuay", "Bolivar"="Bolivar", "Chimborazo"="Chimborazo"),
+                                       selected = "Bolivar"),
+                                       
+                   checkboxGroupInput("checkCity", label = h3("City"), 
+                                      #citiees_vec <- unique(fav_df$city) %>% sort()
+                                      choices = list("Ambato" = "Ambato", "Babahoyo" = "Babahoyo"),
+                                      selected = "Babahoyo"),
+                                       
+                    checkboxGroupInput("checkStores", label = h3("Store"), 
+                                       # unique(fav_df$store_nbr) %>% sort()
+                                       choices = list("1" = 1, "2" = 2, "3" = 3, "4" = 4, "5" = 5,
+                                                      "6" = 6, "7" = 7, "53" = 53, "54" = 54),
+                                       selected = 3),
+                    
+                   checkboxGroupInput("checkStoreType", label = h3("Store Type"), 
+                                      #store_type_vec <- unique(fav_df$type.x) %>% sort()
+                                      choices = c("A", "B", "C" , "D" , "E"),
+                                      #choices = list("A" = "A", "B" = "B", "C" = "C", "D" = "D", "E" = "E"),
+                                      selected = "D"),
+                                        
+                   
+                    checkboxGroupInput("checkClusters", label = h3("Cluster"), 
+                                       # unique(fav_df$cluster) %>% sort()
+                                       choices = list("1" = 1, "2" = 2, "3" = 3, "4" = 4, "5" = 5,
+                                                      "6" = 6, "7" = 7, "9"),
+                                       selected = 3),
+                    
+                    dateRangeInput('dateRange',
+                                   label = 'Date range input: yyyy-mm-dd',
+                                   start = "2016-01-01", end = "2017-07-01"
+                    ),
+                
+                
+                # box(status = "warning",
+                #     selectInput("interval", "Refresh interval",
+                #                 choices = c(
+                #                     "30 seconds" = 30,
+                #                     "1 minute" = 60,
+                #                     "2 minutes" = 120,
+                #                     "5 minutes" = 300,
+                #                     "10 minutes" = 600
+                #                 ),
+                #                 selected = "60"
+                #     ),
+                #     uiOutput("timeSinceLastUpdate"),
+                #     actionButton("refresh", "Refresh now"),
+                #     p(class = "text-muted",
+                #       br(),
+                #       "Source data updates every 30 seconds."
+                #     )
+                # )
+            ),
+                # Show a plot of the generated distribution
+                mainPanel(
+                   plotOutput("distPlot"),
+                   plotOutput("transPlot"), 
+                   plotOutput("clusterPlot")
+                )
+            )
+        ), 
+        tabPanel("Prediction (beta)",
+                 verbatimTextOutput("Regression Based Prediction - select method")
+        ),
+)
+
+# Define server logic ----------------------------------------------------------
+server <- function(input, output) {
+    
+    # Generate Output ----------------------------------------------------------
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+    
+    # Generate Store Plot Output -----------------------------------------------
+    output$transPlot <-  renderPlot({
+        
+        #- Selected Stores
+        stores_use = as.integer(input$checkStores)
+        
+        #- Selected Time Frame
+        start_date = input$dateRange[1]
+        end_date = input$dateRange[2]
+        
+        # start_date = "2016-01-01"
+        # end_date = "2016-12-31"
+        
+        #- Apply Parameters
+        select_stores <- data_transactions %>%
+            filter(date >= as.POSIXct(start_date)) %>%
+            filter(date <= as.POSIXct(end_date)) %>%
+            mutate(date=as.Date(date, format="%Y-%m-%d")) %>%
+            filter(store_nbr %in%  stores_use)
+        
+        #- Plot the result
+        ggplot(select_stores, 
+               aes(x=date, y=transactions, group=store_nbr, col=store_nbr)
+        ) + 
+            geom_line()
+    })
+    # Generate Store Plot Output -----------------------------------------------
+    output$clusterPlot <-  renderPlot({
+        
+    clusters_use = as.integer(input$checkClusters)
+    
+    #- Selected Time Frame
+    start_date = input$dateRange[1]
+    end_date = input$dateRange[2]
+    
+    #- Apply Parameters
+    select_clusters <- merge(x=data_transactions, y=data_stores, y.all=TRUE) %>%
+        filter(date >= as.POSIXct(start_date)) %>%
+        filter(date <= as.POSIXct(end_date)) %>%
+        mutate(date=as.Date(date, format="%Y-%m-%d")) %>%
+        filter(cluster %in%  clusters_use) %>%
+        group_by(cluster) %>%
+        group_by(date, .add=TRUE) %>%
+        summarise(total_trans = sum(transactions)) #%>%
+    #filter(cluster == 4)
+    
+    #- Plot the result
+    ggplot(select_clusters, 
+           aes(x=date, y=total_trans, group = cluster, colour=cluster)) + 
+        geom_line() + geom_point()
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
